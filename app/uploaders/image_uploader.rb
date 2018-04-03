d3614352f341467a319513729f2684c2d13ede10
Jason Su <kittyflat@gmail.com>
2018-04-03 15:31:58 +0800

    Override generate_location

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,6 +5,14 @@ class ImageUploader < Shrine
   plugin :validation_helpers
   plugin :remove_invalid
 
+  def generate_location(io, context)
+    record = context[:record].class.name.downcase if context[:record]
+    attachment_name = context[:name].to_s # name of the attachment attribute on the model
+    style = context[:version].to_s if context[:version]
+    name = super # the default unique identifier
+    [record, attachment_name, style, name].compact.join("/")
+  end
+
   Attacher.validate do
     validate_mime_type_inclusion %w[image/jpeg image/jpg image/png image/pjpeg]
   end
