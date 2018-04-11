818059e063c3f6fb0a3eab8914efd7ed9ceba852
Jason Su <kittyflat@gmail.com>
2018-04-11 12:44:04 -0700

    Add aws-sdk-s3 gem, use AWS storage, update gems

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,11 +1,27 @@
 require "shrine"
 require "shrine/storage/file_system"
 
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"), # permanent
+# File system
+# Shrine.storages = {
+#   cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
+#   store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"), # permanent
+# }
+
+require "shrine/storage/s3"
+
+s3_options = {
+  access_key_id:     Rails.application.secrets.aws_access_key_id,
+  secret_access_key: Rails.application.secrets.aws_secret_access_key,
+  bucket:            "shrine-sandbox-development",
+  region:            "us-west-1",
 }
 
+Shrine.storages = {
+  cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+  store: Shrine::Storage::S3.new(prefix: "store", **s3_options),
+}
+
+# Plugins
 Shrine.plugin :activerecord # or :sequel
 Shrine.plugin :cached_attachment_data # for forms
 # Shrine.plugin :rack_file # for non-Rails apps
