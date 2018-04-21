d380030a5b5002282b04f978d02c670e7f1abda9
Jason Su <kittyflat@gmail.com>
2018-04-21 15:47:23 -0700

    More validations for ImageUploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,10 @@
 class ImageUploader < Shrine
 
+  ALLOWED_EXTENSIONS = %w[jpg jpeg png]
   ALLOWED_TYPES = %w[image/jpeg image/jpg image/png image/pjpeg]
   MAX_SIZE = 10*1024*1024 # 10 MB
+  MIN_SIZE = 10*1024 # 10 KB
+  MAX_DIMENSIONS = 10_000
 
   plugin :store_dimensions
   plugin :processing
@@ -20,6 +23,11 @@ class ImageUploader < Shrine
 
   Attacher.validate do
     validate_max_size MAX_SIZE
-    validate_mime_type_inclusion ALLOWED_TYPES
+    validate_min_size MIN_SIZE
+    validate_extension_inclusion ALLOWED_EXTENSIONS
+    if validate_mime_type_inclusion ALLOWED_TYPES
+      validate_max_width MAX_DIMENSIONS
+      validate_max_height MAX_DIMENSIONS
+    end
   end
 end
